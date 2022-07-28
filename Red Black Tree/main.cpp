7bfd1b3ce08528c552467bc04e00227a9094ad8f
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-07-28 15:48:31 +0600

    commit rbt

---
+++ Red Black Tree/main.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+
+#include "rbtree.h"
+using namespace std;
+
+int main() {
+    RBT<int> tree;
+    ifstream in;
+    in.open("in.txt");
+    ofstream out;
+    out.open("out.txt");
+    int n;
+    in >> n;
+    // cout << n << endl;
+    out << n << endl;
+    for (int tc = 1; tc <= n; tc++) {
+        int e, x;
+        in >> e >> x;
+        int res = 0;
+        if (e == 0) {
+            // delete
+            res = tree.remove(x);
+        } else if (e == 1) {
+            // insert
+            res = tree.insert(x);
+        } else if (e == 2) {
+            // search
+            res = tree.search(x);
+        } else if (e == 3) {
+            // lower count
+            res = tree.lower_count(x);
+        }
+        // cout << tc << endl;
+        // cout << e << " " << x << " " << res << endl;
+        out << e << " " << x << " " << res << endl;
+    }
+    in.close();
+    out.close();
+    return 0;
+}
